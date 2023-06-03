27aac96ef578fd0c0e567dee9ebb4680e1798cb9
CLEM\konst <konstantytadeusz@gmail.com>
2023-06-03 03:27:33 +0200

    JEBAĆ TO GÓWNO
    Signed-off-by: CLEM\konst <konstantytadeusz@gmail.com>

--- Assets/_Scripts/PlayerAttack.cs
+++ Assets/_Scripts/PlayerAttack.cs
@@ -9,17 +9,24 @@ public class PlayerAttack : MonoBehaviour
     bool isAttacking = false;
     [SerializeField] Vector3 attackSize = new Vector3(1.5f, 1.5f, 1.5f);
     public int attackDamage;
-    [SerializeField] GameObject Wepon;
-    Collider colliderl;
+    
     void OnAttack(InputValue value)
     {
-        isAttacking = true;
-        AttackInput = new Vector3(value.Get<Vector2>().x, 0, value.Get<Vector2>().y);
+        //isAttacking = true;
+        //AttackInput = new Vector3(value.Get<Vector2>().x, 0, value.Get<Vector2>().y);
+        //{
+        //    RaycastHit hit;
+        //    if (Physics.Raycast(transform.position+AttackInput,transform.forward,out hit))
+        //    {
+        //        Debug.Log("hit");
+        //        // Check if the hit object has an Enemy script attached
+        //        EnemyHealth enemy = hit.collider.GetComponent<EnemyHealth>();
+        //        if (enemy != null)
+        //        {
+        //            enemy.GetDamage(attackDamage);
+        //        }
+        //    }
+        //}
     }
-    private void Update()
-    {
-        if (isAttacking)
-        {
-        }
-    }
+
 }
