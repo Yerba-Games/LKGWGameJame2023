3fbbc603431e478bf12d6350f5994c28ce3c62d9
CLEM\konst <konstantytadeusz@gmail.com>
2023-06-03 12:26:14 +0200

    PauseMenu Prefab
    Signed-off-by: CLEM\konst <konstantytadeusz@gmail.com>

---
+++ Assets/_Scripts/Sound.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound : MonoBehaviour
+{
+    public static Sound Instance;
+
+    [SerializeField]
+    private AudioSource source;
+    public AudioClip jump;
+
+    public AudioClip dash;
+    public AudioClip spawn;
+    public AudioClip attack;
+    public AudioClip door;
+
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    public static void PlayAttack()
+    {
+        Sound.Play(Instance.attack);
+    }
+    public static void PlayDoor()
+    {
+        Sound.Play(Instance.door);
+    }
+    public static void PlayDash()
+    {
+        Sound.Play(Instance.dash);
+    }
+    public static void PlaySpawn()
+    {
+        Sound.Play(Instance.spawn);
+    }
+    public static void PlayMenuMusic()
+    {
+        Sound.Play(Instance.menumusic);
+    }
+}
